afb5945e6ad5259b071c96f3d56eb3d5ac461a0d
Brad Hesson <brad.hesson@outlook.com>
2023-05-03 02:14:29 -0600

    clippy

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,19 @@
-use std::net::{IpAddr, Ipv4Addr};
 use futures::{stream, StreamExt};
+use std::net::IpAddr;
 
 #[tokio::main]
 async fn main() {
     // console_subscriber::init();
     let client = surge_ping::Client::new(&surge_ping::Config::default()).unwrap();
     stream::iter(0..u32::MAX)
-        .for_each_concurrent(256*256, |i| {
+        .for_each_concurrent(256 * 256, |i| {
             let client = &client;
             async move {
                 let addr = IpAddr::V4(i.into());
                 let mut pinger = client.pinger(addr, 0.into()).await;
                 let reply = pinger.ping(0.into(), &[]).await;
-                match reply {
-                    Ok(reply) => println!("{addr} : {:?}", reply.1),
-                    Err(_) => {}
+                if let Ok(reply) = reply {
+                    println!("{addr} : {:?}", reply.1)
                 }
             }
         })
