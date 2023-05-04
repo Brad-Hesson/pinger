7d859486b83a97f32c5ce6421a75e49cb0d22a35
bradhesson <brad.hesson@outlook.com>
2023-05-03 19:33:45 -0600

    parallel tasks

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,34 @@
-use futures::{stream, StreamExt};
-use std::net::IpAddr;
+use futures::{stream, StreamExt, future::join_all};
+use itertools::Itertools;
+use std::{net::IpAddr, time::Duration};
 
 #[tokio::main]
 async fn main() {
-    // console_subscriber::init();
+    console_subscriber::init();
+    let n = 4;
+    let part = u32::MAX / n;
+    let mut handles = vec![];
+    let mut num_done = 0;
+    for (a, b) in (0..n)
+        .map(|i| i * part)
+        .chain(Some(u32::MAX))
+        .tuple_windows()
+    {
+        let h = tokio::spawn(task(a..b));
+        handles.push(h);
+    }
+    join_all(handles).await;
+}
+
+async fn task(i: impl IntoIterator<Item = u32>) {
     let client = surge_ping::Client::new(&surge_ping::Config::default()).unwrap();
-    stream::iter(0..u32::MAX)
-        .for_each_concurrent(256 * 256, |i| {
+    stream::iter(i)
+        .for_each_concurrent(100_000, |i| {
             let client = &client;
             async move {
                 let addr = IpAddr::V4(i.into());
                 let mut pinger = client.pinger(addr, 0.into()).await;
+                pinger.timeout(Duration::from_secs(5));
                 let reply = pinger.ping(0.into(), &[]).await;
                 if let Ok(reply) = reply {
                     println!("{addr:<15} : {:?}", reply.1)
