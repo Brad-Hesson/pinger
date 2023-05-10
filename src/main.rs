39832fd98673be2ed8e55532a4b3ac6eb2a1a1da
Brad Hesson <brad.hesson@outlook.com>
2023-05-10 09:26:35 -0600

    changed to using one task per ping

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,7 @@
 use clap::Parser;
-use futures::{
-    future::{join_all, select},
-    stream, StreamExt,
-};
-use itertools::Itertools;
+use ipnet::Ipv4Net;
+use iprange::IpRange;
 use std::{
-    net::IpAddr,
     sync::{
         atomic::{AtomicUsize, Ordering},
         Arc,
@@ -16,70 +12,67 @@ use std::{
 #[tokio::main]
 async fn main() {
     console_subscriber::init();
-    let args = Args::parse();
-    let n = args.num_threads;
+    let mut range = IpRange::<Ipv4Net>::new();
+    range.add("0.0.0.0/0".parse().unwrap());
+    // range.add("142.244.0.0/16".parse().unwrap());
+    // range.add("129.128.0.0/16".parse().unwrap());
 
+    let ips = range.into_iter().flat_map(|net| net.hosts());
     let state = Arc::new(State::new());
+    let client = Arc::new(surge_ping::Client::new(&surge_ping::Config::default()).unwrap());
 
-    let max = u32::MAX;
-    let part = max / n;
-    let workers_h = join_all(
-        (0..n)
-            .map(|i| i * part)
-            .chain(Some(max))
-            .tuple_windows()
-            .map(|(a, b)| tokio::spawn(worker(a..b, state.clone()))),
-    );
-    let printer_h = tokio::spawn(printer(state));
-
-    select(workers_h, printer_h).await;
+    tokio::spawn(printer(state.clone()));
+    for addr in ips {
+        let mut pinger = client.pinger(addr.into(), 0.into()).await;
+        pinger.timeout(Duration::from_secs(5));
+        state.running.fetch_add(1, Ordering::Release);
+        tokio::spawn(worker(pinger, state.clone()));
+        while state.running.load(Ordering::Acquire) >= 100_000 {
+            tokio::task::yield_now().await;
+        }
+    }
 }
 
 async fn printer(state: Arc<State>) {
     let mut last_time = Instant::now();
     let mut last_value = 0;
     loop {
-        let done = state.num_done.load(Ordering::Relaxed);
-        // let replied = state.num_replied.load(Ordering::Relaxed);
-        let percentage = done as f64 / u32::MAX as f64 * 100.;
+        let done = state.num_done.load(Ordering::Acquire);
+        let replied = state.num_replied.load(Ordering::Acquire);
+        let active = state.running.load(Ordering::Acquire);
+        let perc_done = done as f64 / u32::MAX as f64 * 100.;
+        let perc_replied = replied as f64 / done as f64 * 100.;
         let now = Instant::now();
         let rate = (done - last_value) as f64 / (now - last_time).as_secs_f64();
         last_time = now;
         last_value = done;
-        println!("{percentage:.3}%  @  {rate:.2} p/s",);
-        tokio::time::sleep(Duration::from_secs(10)).await;
+        println!(
+            "{perc_done:.3}% done | {perc_replied:.3}% replied | {rate:.2} p/s | {active} active",
+        );
+        tokio::time::sleep(Duration::from_secs(1)).await;
     }
 }
 
-async fn worker(i: impl IntoIterator<Item = u32>, state: Arc<State>) {
-    let client = surge_ping::Client::new(&surge_ping::Config::default()).unwrap();
-    stream::iter(i)
-        .for_each_concurrent(100_000, |i| {
-            let client = &client;
-            let state = &state;
-            async move {
-                let addr = IpAddr::V4(i.into());
-                let mut pinger = client.pinger(addr, 0.into()).await;
-                pinger.timeout(Duration::from_secs(5));
-                let reply = pinger.ping(0.into(), &[]).await;
-                if let Ok(_) = reply {
-                    state.num_replied.fetch_add(1, Ordering::Relaxed);
-                }
-                state.num_done.fetch_add(1, Ordering::Relaxed);
-            }
-        })
-        .await;
+async fn worker(mut pinger: surge_ping::Pinger, state: Arc<State>) {
+    let reply = pinger.ping(0.into(), &[]).await;
+    if let Ok(_) = reply {
+        state.num_replied.fetch_add(1, Ordering::Release);
+    }
+    state.num_done.fetch_add(1, Ordering::Release);
+    state.running.fetch_sub(1, Ordering::Release);
 }
 
 struct State {
     num_done: AtomicUsize,
     num_replied: AtomicUsize,
+    running: AtomicUsize,
 }
 impl State {
     fn new() -> Self {
         Self {
             num_done: AtomicUsize::new(0),
             num_replied: AtomicUsize::new(0),
+            running: AtomicUsize::new(0),
         }
     }
 }
