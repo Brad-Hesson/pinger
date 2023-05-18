3d06aecb26d868539b08ce71f9e95837d3b1a2ed
Brad Hesson <brad.hesson@outlook.com>
2023-05-18 15:59:19 -0600

    more comments

--- src/ping.rs
+++ src/ping.rs
@@ -20,7 +20,7 @@ use tokio::{
 const DATA_SIZE: u64 = std::mem::size_of::<f32>() as u64;
 
 pub async fn main(args: Args) {
-    // Construct the collection of subnets from the arguments struct
+    // Construct the collection of subnets from the cli arg
     let mut range = IpRange::<Ipv4Net>::new();
     for net_string in args.subnets {
         range.add(net_string.parse().unwrap());
@@ -55,7 +55,7 @@ pub async fn main(args: Args) {
     // and remaining number of addresses to screen.
     let total_num_addrs = range.into_iter().flat_map(|net| net.hosts()).count();
     println!("{total_num_addrs} addresses to ping in total");
-    println!("{num_done} addresses already in file");
+    println!("{num_done} addresses already in the file");
 
     // Construct the shared state struct and the pinger client struct.
     let state = Arc::new(State::new(total_num_addrs as u64, num_done));
@@ -69,7 +69,7 @@ pub async fn main(args: Args) {
     let file_writer_handle = tokio::spawn(file_writer(rx, BufWriter::new(file)));
 
     // Spawn the stats printer task, which is given a reference to the shared state and the update
-    // interval from the command line args.
+    // interval from the cli arg.
     let stats_printer_handle = tokio::spawn(stats_printer(
         state.clone(),
         Duration::from_secs(args.update_interval),
@@ -79,7 +79,7 @@ pub async fn main(args: Args) {
     for addr in addrs {
         // Construct a pinger
         let mut pinger = client.pinger(addr.into(), 0.into()).await;
-        // Set the timout based on the command line argument
+        // Set the timout based on the cli arg
         pinger.timeout(Duration::from_secs(args.timeout));
         // Add 1 to the running count
         state.num_running.fetch_add(1, Ordering::Release);
@@ -87,13 +87,13 @@ pub async fn main(args: Args) {
         let handle = tokio::spawn(ping_worker(pinger, state.clone()));
         // Send the worker handle to the file writer
         tx.send(handle).unwrap();
-        // wait until the running count drops below the max threshold given in the commmand line args
+        // Wait until the running count drops below the max threshold given in the cli arg
         while state.num_running.load(Ordering::Acquire) >= args.num_concurrent {
             tokio::task::yield_now().await;
         }
     }
 
-    // Now that all the pings have been sent, drop the channel sender in order to signal the file writer
+    // Now that all the pings have been sent, drop the channel sender in order to signal to the file writer
     // that no more handles will be added to the message queue and it can exit once it is done.
     drop(tx);
 
@@ -107,7 +107,7 @@ async fn file_writer(
     mut file: BufWriter<File>,
 ) {
     // As long as there is another ping worker handle in the message queue:
-    // Wait for the ping to return, or timeout.
+    // Wait for the ping to either return or timeout.
     while let Some(handle) = rx.recv().await {
         // Get the duration of the ping, or use -1 for a timeout.
         let num = match handle.await.unwrap() {
@@ -122,18 +122,26 @@ async fn file_writer(
 }
 
 async fn stats_printer(state: Arc<State>, interval: Duration) {
+    // Set the baseline time and progress value
     let mut last_time = Instant::now();
     let mut last_value = state.num_done.load(Ordering::Acquire);
     loop {
+        // Wait for the interval specified in the cli args
         tokio::time::sleep(interval).await;
+        // Get the current time, progress value, and running count
         let now = Instant::now();
         let done = state.num_done.load(Ordering::Acquire);
         let active = state.num_running.load(Ordering::Acquire);
+        // Calculate the progress percentage and the ping rate
         let perc_done = done as f64 / state.total as f64 * 100.;
         let rate = (done - last_value) as f64 / (now - last_time).as_secs_f64();
+        // Set the baselines to the current values
         last_time = now;
         last_value = done;
+        // Print the results
         println!("{perc_done:>7.3}% done | {rate:>9.2} p/s | {active:>6} active",);
+        // If the number completed is equal to the total number of addrs, then we must
+        // be done, so we exit the loop
         if done == state.total {
             break;
         }
@@ -143,7 +151,7 @@ async fn stats_printer(state: Arc<State>, interval: Duration) {
 async fn ping_worker(mut pinger: surge_ping::Pinger, state: Arc<State>) -> Option<Duration> {
     // Start the ping and await its return.
     let reply = pinger.ping(0.into(), &[]).await;
-    // Now the that the ping has returned, add one to num_done and subtract one from the running count
+    // Now that the ping has returned, add 1 to num_done and subtract 1 from the running count
     state.num_done.fetch_add(1, Ordering::Release);
     state.num_running.fetch_sub(1, Ordering::Release);
     // Return an optional duration based on if the ping timed out or returned successfully.
@@ -178,16 +186,16 @@ impl State {
 
 #[derive(Debug, clap::Args)]
 pub struct Args {
-    #[arg(default_value_t = 100_000, short, long)]
     /// The maximuim number of concurrent pings
+    #[arg(default_value_t = 100_000, short, long)]
     num_concurrent: usize,
-    #[arg(default_value_t = 1, short, long)]
     /// The interval for printings stats in seconds
+    #[arg(default_value_t = 1, short, long)]
     update_interval: u64,
-    #[arg(default_value_t = 2, short, long)]
     /// The ping timeout in seconds
+    #[arg(default_value_t = 2, short, long)]
     timeout: u64,
-    #[arg(default_values_t = vec!["142.244.0.0/16".to_string(), "129.128.0.0/16".to_string()], short, long)]
     /// The subnets to ping
+    #[arg(default_values_t = vec!["142.244.0.0/16".to_string(), "129.128.0.0/16".to_string()], short, long)]
     subnets: Vec<String>,
 }
